81c157dca4b9ec723af5aebc7a819b847d2b44f2
sahoosagar45-hak <sahoosagar8.95@gmail.com>
2026-09-10 23:03:19 +0530

    Time: 3 ms (95.39%) | Memory: 71.7 MB (30.93%) - LeetSync

--- 4412-count-good-cyclic-rotations/count-good-cyclic-rotations.java
+++ 4412-count-good-cyclic-rotations/count-good-cyclic-rotations.java
@@ -2,24 +2,36 @@ class Solution {
     public int countGoodRotations(int[] nums) {
         int n = nums.length;
         int half = n / 2;
-        int total_sum = 0;
-        int count = 0;
-        int leftHalf_sum = 0;
 
-        for(int num : nums){
+        long total_sum = 0;
+        long leftHalf_sum = 0;
+        int count = 0;
+
+        // Total sum
+        for (int num : nums) {
             total_sum += num;
         }
-        for(int i = 0; i < half; i++){
+
+        // Sum of first half
+        for (int i = 0; i < half; i++) {
             leftHalf_sum += nums[i];
         }
 
-        for(int i = 0; i < n; i++){
-            int rightHalf_sum = total_sum - leftHalf_sum;
-            if(leftHalf_sum > rightHalf_sum){
+        // Check every rotation
+        for (int i = 0; i < n; i++) {
+
+            long rightHalf_sum = total_sum - leftHalf_sum;
+
+            if (leftHalf_sum > rightHalf_sum) {
                 count++;
             }
-            leftHalf_sum = leftHalf_sum - nums[i] + nums[(i+half) % n];
+
+            // Slide the window
+            leftHalf_sum =
+                leftHalf_sum - nums[i]
+                + nums[(i + half) % n];
         }
+
         return count;
     }
 }
